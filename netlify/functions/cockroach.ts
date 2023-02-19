eb81cf0324dbd26724ce5f50360eb75ebac55c0c
Lockevn <lockevn@gmail.com>
2023-02-19 16:32:15 +0700

    netlify function: add cockroach

---
+++ netlify/functions/cockroach.ts
@@ -0,0 +1,30 @@
+import { Client } from "pg" 
+
+// $env:DATABASE_URL = "postgresql://dbadmin:cwKPcdBvwkFsX9Sd4RiNFA@qenglish-3881.6xw.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+const connectionString =  "postgresql://dbadmin:cwKPcdBvwkFsX9Sd4RiNFA@qenglish-3881.6xw.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+
+// const client = new Client(process.env.DATABASE_URL);
+const client = new Client(connectionString);
+
+(async () => {
+  await client.connect();
+  try {
+    const results = await client.query("SELECT NOW()");
+    const inserts = await client.query(`INSERT INTO public.testtable (student)
+    VALUES ('${Date.now()}');
+  `);
+    console.log(results);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(inserts)
+    }
+
+  } catch (err) {
+    console.error("error executing query:", err);
+  } finally {
+    client.end();
+  }
+})();
+
+
