689f24d75c9c9c35ecbb437fe2c3b211b041106b
Lockevn <lockevn@gmail.com>
2023-02-19 16:49:45 +0700

    function: change syntax

--- netlify/functions/cockroach.ts
+++ netlify/functions/cockroach.ts
@@ -1,30 +1,29 @@
-import { Client } from "pg" 
+import { Client } from 'pg';
 
-// $env:DATABASE_URL = "postgresql://dbadmin:cwKPcdBvwkFsX9Sd4RiNFA@qenglish-3881.6xw.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
-const connectionString =  "postgresql://dbadmin:cwKPcdBvwkFsX9Sd4RiNFA@qenglish-3881.6xw.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+export async function handler(event, context) {
+  // $env:DATABASE_URL = "postgresql://dbadmin:cwKPcdBvwkFsX9Sd4RiNFA@qenglish-3881.6xw.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+  const connectionString =
+    'postgresql://dbadmin:cwKPcdBvwkFsX9Sd4RiNFA@qenglish-3881.6xw.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full';
 
-// const client = new Client(process.env.DATABASE_URL);
-const client = new Client(connectionString);
+  // const client = new Client(process.env.DATABASE_URL);
+  const client = new Client(connectionString);
 
-(async () => {
   await client.connect();
   try {
-    const results = await client.query("SELECT NOW()");
-    const inserts = await client.query(`INSERT INTO public.testtable (student)
-    VALUES ('${Date.now()}');
-  `);
+    const results = await client.query('SELECT NOW()');
     console.log(results);
+
+    const query = 'INSERT INTO public.testtable (student) VALUES ($1)';
+    const values = [Date.now()];
+    let inserts = await client.query(query, values);
 
     return {
       statusCode: 200,
-      body: JSON.stringify(inserts)
-    }
-
+      body: JSON.stringify(inserts),
+    };
   } catch (err) {
-    console.error("error executing query:", err);
+    console.error('error executing query:', err);
   } finally {
     client.end();
   }
-})();
-
-
+}
